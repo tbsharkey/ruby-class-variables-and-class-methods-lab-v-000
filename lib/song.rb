6b6182d0a33843980302fbb1f9f46e8b68aafe8c
Tom Sharkey <tbsharkey@gmail.com>
2021-05-27 00:27:05 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genre = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+
+  end
+
+  def self.artist_count
+
+  end
+
+  def self.genres
+
+  end
+
+  def self.genre_count
+
+  end
+
+end
